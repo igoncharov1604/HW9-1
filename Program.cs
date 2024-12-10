51900e1f0ab2171f2e431e318704f5f6d12f508d
igoncharov1604 <i.goncharov@istu.edu.ua>
2024-12-10 18:27:00 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Лямбда-вирази для кожного випадку
+        Func<double, double> positiveCase = x => 1 / x;
+        Func<double, double> negativeCase = x => x * x + 2 * x + 4;
+        Func<double, double> zeroCase = x => 4.0;
+
+        // Введення значення x
+        Console.Write("Введіть значення x: ");
+        if (double.TryParse(Console.ReadLine(), out double x))
+        {
+            double result;
+
+            // Обчислення значення F(x) залежно від x
+            if (x > 0)
+            {
+                result = positiveCase(x);
+            }
+            else if (x < 0)
+            {
+                result = negativeCase(x);
+            }
+            else
+            {
+                result = zeroCase(x);
+            }
+
+            // Вивід результату
+            Console.WriteLine($"F({x}) = {result}");
+        }
+        else
+        {
+            Console.WriteLine("Введено некоректне число.");
+        }
+    }
+}
